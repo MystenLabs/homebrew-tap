9a04937afa0ea50ce43ac5b5edf28e370332daa9
johnjmartin <johnjmartin@users.noreply.github.com>
2023-12-07 00:30:40 +0000

    Update Homebrew formula for release testnet-v1.15.0

--- Formula/sui.rb
+++ Formula/sui.rb
@@ -1,22 +1,21 @@
-# homebrew-mylocaltap/Formula/<formula>.rb
 class Sui < Formula
   desc "Sui CLI install"
   homepage "https://docs.sui.io"
-  version "1.14.2"
+  version "testnet-v1.15.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-mainnet-v1.14.2-macos-arm64.tgz"
-      sha256 "660504c3e36a6e2aa0c975d230cfdfde5197c112d6a4750f340399f017dfe066"
+      url "https://github.com/MystenLabs/sui/releases/sui-mainnet-vtestnet-v1.15.0-macos-arm64.tgz"
+      sha256 "sha256tmp1"
 
       def install
         bin.install "target/release/sui-macos-arm64" => "sui"
       end
     end
     if Hardware::CPU.intel?
-      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-mainnet-v1.14.2-macos-x86_64.tgz"
-      sha256 "bde8e930f46e0cad80f09a0022986e6719871c52fbf923bcb048be3a922d7b9c"
+      url "https://github.com/MystenLabs/sui/releases/sui-mainnet-vtestnet-v1.15.0-macos-x86_64.tgz"
+      sha256 "sha256tmp2"
 
       def install
         bin.install "target/release/sui-macos-x86_64" => "sui"
@@ -26,8 +25,8 @@ class Sui < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-mainnet-v1.14.2-ubuntu-x86_64.tgz"
-      sha256 "56dd25625db4d705ef0edc2acc3db698a0184ecc5e25203ec0cb5e0e84d513ef"
+      url "https://github.com/MystenLabs/sui/releases/sui-mainnet-vtestnet-v1.15.0-ubuntu-x86_64.tgz"
+      sha256 "xyz"
 
       def install
         bin.install "target/release/sui-ubuntu-x86_64" => "sui"
@@ -39,5 +38,3 @@ class Sui < Formula
     system "#{bin}/sui --version"
   end 
 end
-
-
