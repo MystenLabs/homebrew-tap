7a417048d921710b9f52931145030ada40e83e04
ebmifa <ebmifa@users.noreply.github.com>
2024-01-17 00:38:35 +0000

    Update Homebrew formula for release mainnet-v1.16.2

--- Formula/sui.rb
+++ Formula/sui.rb
@@ -1,21 +1,21 @@
 class Sui < Formula
   desc "Sui CLI install"
   homepage "https://docs.sui.io"
-  version "1.15.1"
+  version "1.16.2"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-mainnet-v1.15.1-macos-arm64.tgz"
-      sha256 "706a156ff17de95a444f806aa0e0f5f2c65c4f3b5286ba0afb1309b8fc4e29b7"
+      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-mainnet-v1.16.2-macos-arm64.tgz"
+      sha256 "d0bc062f2aa24a05c20c3ccb9f467064e577886c316c9294a8b68c81f9da7842"
 
       def install
         bin.install "target/release/sui-macos-arm64" => "sui"
       end
     end
     if Hardware::CPU.intel?
-      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-mainnet-v1.15.1-macos-x86_64.tgz"
-      sha256 "7739a2e8d795635590a369bdb91105e39b6787fd1f6b419ef47db59712b632ab"
+      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-mainnet-v1.16.2-macos-x86_64.tgz"
+      sha256 "7ea08f9947fce60bd6464cb848515be963ad8d392c4d08e771824af00fa741cb"
 
       def install
         bin.install "target/release/sui-macos-x86_64" => "sui"
@@ -25,8 +25,8 @@ class Sui < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-mainnet-v1.15.1-ubuntu-x86_64.tgz"
-      sha256 "7fda445a9410992c7a4a909ce60602386793de92702b1f3a3c0dac491232c3bf"
+      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-mainnet-v1.16.2-ubuntu-x86_64.tgz"
+      sha256 "cc525fe55408a71b42a1777cd0ce6c8e58d4f1be159f2e7290d1d98e71a1e1a2"
 
       def install
         bin.install "target/release/sui-ubuntu-x86_64" => "sui"
