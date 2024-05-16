392b0b70926140ea15c8655cd78c6d22ffd51bc5
ebmifa <ebmifa@users.noreply.github.com>
2024-05-16 20:41:35 +0000

    Update Homebrew formula for release testnet-v1.25.0

--- Formula/sui.rb
+++ Formula/sui.rb
@@ -9,7 +9,7 @@ class Sui < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-testnet-v1.25.0-macos-arm64.tgz"
-      sha256 "fde7ed5a2c700fa4a0c54dd47d06d1f05a7dd7bb29c06bbdf6b129a6603421e8"
+      sha256 "2e8829e29796c418dd3fd137a968669e22eaa47cf40a19d92346a4855776c288"
 
       def install
         bin.install "target/release/sui-macos-arm64" => "sui"
@@ -17,7 +17,7 @@ class Sui < Formula
     end
     if Hardware::CPU.intel?
       url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-testnet-v1.25.0-macos-x86_64.tgz"
-      sha256 "62d62d8725d42009e4edbc1cf8b0136a2e7f974a1f3c1eae3520fcbe57d5ca2f"
+      sha256 ""
 
       def install
         bin.install "target/release/sui-macos-x86_64" => "sui"
@@ -28,7 +28,7 @@ class Sui < Formula
   on_linux do
     if Hardware::CPU.is_64_bit?
       url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-testnet-v1.25.0-ubuntu-x86_64.tgz"
-      sha256 "37d23ab6f31feb85629a5dfd69848828e2b94d28583ae0abe328a6e6ad611e64"
+      sha256 ""
 
       def install
         bin.install "target/release/sui-ubuntu-x86_64" => "sui"
