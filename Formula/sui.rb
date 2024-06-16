53fa4bb4e4645ac7d4873f5abf4b1b8376b59557
pei-mysten <pei-mysten@users.noreply.github.com>
2024-06-16 19:48:12 +0000

    Update Homebrew formula for release testnet-v1.27.2

--- Formula/sui.rb
+++ Formula/sui.rb
@@ -1,23 +1,23 @@
 class Sui < Formula
   desc "Sui CLI install"
   homepage "https://docs.sui.io"
-  version "1.27.0"
+  version "1.27.2"
   license "Apache-2.0"
 
   deprecate! date: "2024-02-05", because: "migrated to homebrew-core, untap and install via homebrew core: `brew untap mystenlabs/tap && brew install sui`"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-testnet-v1.27.0-macos-arm64.tgz"
-      sha256 "46d8cdfa27b3b40ab1a27b9271621bb4550cfa9f03f73ba26aef95aad9941080"
+      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-testnet-v1.27.2-macos-arm64.tgz"
+      sha256 "c41f3c7ca6272f5b37cdaab62d4c4617415d20c0e4e44f8e8d221989032508bc"
 
       def install
         bin.install "target/release/sui-macos-arm64" => "sui"
       end
     end
     if Hardware::CPU.intel?
-      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-testnet-v1.27.0-macos-x86_64.tgz"
-      sha256 "5edd968e43a8107a67c0e69a7a16f358b5e7070653aa73b442ceb31456a9b0cb"
+      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-testnet-v1.27.2-macos-x86_64.tgz"
+      sha256 "f37f8e7d1926be7a17b95557a9d0ea16bfe21ad674ef81e8f73edd6a7ab603ef"
 
       def install
         bin.install "target/release/sui-macos-x86_64" => "sui"
@@ -27,8 +27,8 @@ class Sui < Formula
 
   on_linux do
     if Hardware::CPU.is_64_bit?
-      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-testnet-v1.27.0-ubuntu-x86_64.tgz"
-      sha256 "5a49533323bc4945c75bc68ec217d0a10c6ecff62b101b7c29223be4a4ef6216"
+      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-testnet-v1.27.2-ubuntu-x86_64.tgz"
+      sha256 "f5055024ad78d9d618446eaa97e0475ee50a8410686a7e6171b4c23c8c2716fe"
 
       def install
         bin.install "target/release/sui-ubuntu-x86_64" => "sui"
