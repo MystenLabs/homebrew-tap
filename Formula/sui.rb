011f49a2bb4bef91824a4183367a4cf2054dceb4
ebmifa <ebmifa@users.noreply.github.com>
2024-09-16 01:04:46 +0000

    Update Homebrew formula for release testnet-v1.33.1

--- Formula/sui.rb
+++ Formula/sui.rb
@@ -1,23 +1,23 @@
 class Sui < Formula
   desc "Sui CLI install"
   homepage "https://docs.sui.io"
-  version "1.33.0"
+  version "1.33.1"
   license "Apache-2.0"
 
   deprecate! date: "2024-02-05", because: "migrated to homebrew-core, untap and install via homebrew core: `brew untap mystenlabs/tap && brew install sui`"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-testnet-v1.33.0-macos-arm64.tgz"
-      sha256 "f17a948d9254edb5d2f649b6171a2d690aead1fd484c1d231a661398ab9c867b"
+      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-testnet-v1.33.1-macos-arm64.tgz"
+      sha256 "172cf16d783b3d13c35736cf037cc0ca7d9fc1c33e495e923fc3cac188983a40"
 
       def install
         bin.install "target/release/sui-macos-arm64" => "sui"
       end
     end
     if Hardware::CPU.intel?
-      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-testnet-v1.33.0-macos-x86_64.tgz"
-      sha256 "7588c123a13a004c53458d41abf3f6846e44a4bbf0e2a565fbeb9a7cd7c0b798"
+      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-testnet-v1.33.1-macos-x86_64.tgz"
+      sha256 "21b5df0de9a706dc56bf5c52d5e5d8f737015b4de4bbe1ad0a62f807d500acc0"
 
       def install
         bin.install "target/release/sui-macos-x86_64" => "sui"
@@ -27,8 +27,8 @@ class Sui < Formula
 
   on_linux do
     if Hardware::CPU.is_64_bit?
-      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-testnet-v1.33.0-ubuntu-x86_64.tgz"
-      sha256 "933cbe81b6a85c18b7a74ec38564d588a2dfd3c8a132840d8ef319442125e1df"
+      url "https://sui-releases.s3-accelerate.amazonaws.com/releases/sui-testnet-v1.33.1-ubuntu-x86_64.tgz"
+      sha256 "c8f567f4f874b25894cebf2698d18fb93dfaaa163aa4c09dddc0663ee03aae00"
 
       def install
         bin.install "target/release/sui-ubuntu-x86_64" => "sui"
